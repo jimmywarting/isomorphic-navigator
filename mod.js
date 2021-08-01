75bb32e716028db7422e3392b18ddce7e915ce54
Jimmy Wärting <jimmy@warting.se>
2021-08-01 17:00:57 +0200

    testing gh actions

--- mod.js
+++ mod.js
@@ -1,9 +1,1 @@
-/*! <package name>. MIT License. Jimmy Wärting <https://jimmy.warting.se/opensource> */
-
-/*
-  1. Search & replace <package name>
-  2. Replace desc in package.json
-  3. Add keywords in package.json
-  4. Update README.md
-  5. start hacking
-*/
+/*! isomorphic-navigator. MIT License. Jimmy Wärting <https://jimmy.warting.se/opensource> */
